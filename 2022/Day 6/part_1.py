660db2c12d5d2ea4b2c536dd0dd3841c9bd217cd
magical-obama <maschwaerzler@a1.net>
2022-12-06 10:22:11 +0100

    Solve Day 6

---
+++ 2022/Day 6/part_1.py
@@ -0,0 +1,33 @@
+input_data = ""
+buf_len = 4
+
+
+with open('input') as f:
+    input_data = f.read()
+
+
+def advance_buffer():
+    global current_index
+    if len(buf) < buf_len:
+        buf.append(input_data[current_index])
+    else:
+        buf.pop(0)
+        buf.append(input_data[current_index])
+    current_index += 1
+
+
+def process_next():
+    if len(buf) < buf_len:
+        advance_buffer()
+    else:
+        if len(set(buf)) == len(buf):
+            return True
+        advance_buffer()
+    return False
+
+buf = []
+current_index = 0
+for i in range(len(input_data)):
+    if process_next():
+        print('Solution:', current_index)
+        break
