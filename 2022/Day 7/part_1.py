08aacd6f4505c88547bd79041987df3118985b75
magical-obama <maschwaerzler@a1.net>
2022-12-07 21:36:12 +0100

    Second try to solve Day 7

--- 2022/Day 7/part_1.py
+++ 2022/Day 7/part_1.py
@@ -18,20 +18,41 @@ class File:
 
 
 class Directory:
-    def __init__(self, name, parent=None):
+    def __init__(self, name):
         self.name: str = name
-        self.parent: str or None = parent
         self.children: [Directory or File] = []
 
     def __str__(self):
         return f"{self.name} (dir)"
 
 
-def get_dir_with_path(path):
+# def get_dir_from_path(path):
+#     global root_dir
+#     current_dir = root_dir
+#     if len(path) == 0:
+#         return root_dir
+#     for path_element in path:
+#         children_of_current_dir = current_dir.children
+#         if len(children_of_current_dir) == 0:
+#             print('Has no children')
+#             exit(-1)
+#         for element in children_of_current_dir:
+#             if isinstance(element, Directory) and element.name == path_element:
+#                 return element
+#             else:
+#                 print("Directory non existent")
+#                 exit(-1)
+
+
+def get_dir_from_path(path: list):
     global root_dir
     current_dir = root_dir
     if len(path) == 0:
         return root_dir
+    if path[0] == '/':
+        if len(path) == 1:
+            return root_dir
+        path.pop(0)
     for path_element in path:
         children_of_current_dir = current_dir.children
         if len(children_of_current_dir) == 0:
@@ -39,14 +60,26 @@ def get_dir_with_path(path):
             exit(-1)
         for element in children_of_current_dir:
             if isinstance(element, Directory) and element.name == path_element:
-                return element
-            else:
-                print("Directory non existent")
-                exit(-1)
+                current_dir = element
+            # else:
+            #     print("Directory non existent")
+            #     exit(-1)
+    return current_dir
 
 
-def parse_cd(cd_command):
-    pass
+def print_file_tree(dir_to_print: list):
+    current_dir = get_dir_from_path(dir_to_print)
+    if len(dir_to_print) == 0:
+        print(f'- {str(current_dir)}')
+    depth = len(dir_to_print) + 1
+    for child in current_dir.children:
+        if isinstance(child, Directory):
+            print(f'{"  " * depth}- {str(child)}')
+            dir_to_print.append(child.name)
+            assert isinstance(dir_to_print, list)
+            print_file_tree(dir_to_print)
+        else:
+            print(f'{"  " * depth}- {str(child)}')
 
 
 with open('input') as f:
@@ -80,21 +113,24 @@ for i in range(len(raw_commands)):
 
 root_dir = Directory("/")
 
-current_dir = []
+current_path = []
 
-print(commands[1])
+# print(commands[1])
 
 for command in commands:
     if command.name == "cd":
         if command.arg == "/":
-            current_dir = []
+            current_path = []
         elif command.arg == "..":
-            current_dir.pop()
+            current_path.pop()
         else:
-            current_dir.append(command.arg)
+            current_path.append(command.arg)
     if command.name == "ls":
         for element in command.output:
-            if element.split()[0] == "dir":
-                get_dir_with_path(current_dir).children.append(Directory(element.split()[1], current_dir[:-1]))
+            output_parts = element.split()
+            if output_parts[0] == "dir":
+                get_dir_from_path(current_path).children.append(Directory(output_parts[1]))
             else:
-                get_dir_with_path(current_dir).children.append(File(element.split()[1], element.split()[0]))
+                get_dir_from_path(current_path).children.append(File(output_parts[1], output_parts[0]))
+
+print_file_tree([])
